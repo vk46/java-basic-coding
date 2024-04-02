acd591ef67ba8585bb71702681ff340d4619583f
Vinay Kumar Palla <vk.hack46@gmail.com>
2024-04-02 13:49:41 +0530

    new changes added

--- src/com/vk/collections/MapFunction.java
+++ src/com/vk/collections/MapFunction.java
@@ -10,13 +10,7 @@ import java.util.stream.Collectors;
 
 public class MapFunction {
     public static void main(String[] args) {
-        Employer e1 = new Employer(1, "Vinay", "Hyderabad", "India", 20000);
-        Employer e2 = new Employer(2, "Ajay", "Hyderabad", "India", 10000);
-        Employer e3 = new Employer(3, "Ravi", "Bangalore", "India", 30000);
-        Employer e4 = new Employer(4, "Niki", "San Jose", "US", 40000);
-        Employer e5 = new Employer(5, "Vrindha", "Bangalore", "India", 50000);
-
-        List<Employer> employeeList = Arrays.asList(e1,e2,e3,e4,e5);
+        List<Employer> employeeList = getEmployers();
         HashMap<Employer, String> map = new HashMap<>();
 
         Map<String, List<Employer>> groupedEmployees = employeeList.stream()
@@ -27,5 +21,15 @@ public class MapFunction {
             System.out.println("Employees in " + city + ": ");
             empList.forEach(emp -> System.out.println(emp.getName()));
         });
+    }
+
+    private static List<Employer> getEmployers() {
+        Employer e1 = new Employer(1, "Vinay", "Hyderabad", "India", 20000);
+        Employer e2 = new Employer(2, "Ajay", "Hyderabad", "India", 10000);
+        Employer e3 = new Employer(3, "Ravi", "Bangalore", "India", 30000);
+        Employer e4 = new Employer(4, "Nikita", "San Jose", "US", 40000);
+        Employer e5 = new Employer(5, "Vrindha", "Bangalore", "India", 50000);
+        Employer e6 = new Employer(4, "Teja", "Bangalore", "India", 40000);
+        return Arrays.asList(e1,e2,e3,e4,e5,e6);
     }
 }
